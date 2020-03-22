c436940c91ced54b15706296970658e6637ae6c5
Juan <lrccfj@outlook.com>
2020-03-22 12:13:02 +0000

    Can add array of items

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-let newItem = [];
+let newItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true }));
@@ -13,17 +13,16 @@ app.get("/", function (req, res) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let dayName = days[today.getDay()];
     res.render("list", { typeOfDay: dayName });*/
-
-
-    var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    var today = new Date();
+    let options = { weekday: 'long', month: 'long', day: 'numeric' };
+    let today = new Date();
     let dayName = today.toLocaleDateString("en-US", options)
-    res.render("list", { typeOfDay: dayName, newItemVar: newItem });
+    res.render("list", { typeOfDay: dayName, newItemVar: newItems });
 });
 
 app.post("/", function(req, res){
 
-    newItem = req.body.itemNew;
+    let newItem = req.body.itemNew;
+    newItems.push(newItem);
     res.redirect("/");
      
 
